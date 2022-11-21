93f764dca5b6d174ab347255068316c012560f19
Adolfo Griego <104970935+AdolfoGriego@users.noreply.github.com>
2022-11-21 15:45:46 -0600

    Add files via upload

--- plugins/nv-hola.js
+++ plugins/nv-hola.js
@@ -1,14 +1,18 @@
 let util = require('util')
 let path = require('path')
 
+
 let handler = async (m, { conn }) => {
-let vn = './src/hola.mp3'
-conn.sendFile(m.chat, vn, 'hola.mp3', null, m, true, {
+let vn = './src/Hola.mp3'
+conn.sendFile(m.chat, vn, 'Hola.mp3', null, m, true, {
 type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /Hi|Hola|hola/
-handler.command = /^(hola|Hola?$)/
+handler.customPrefix = /Hola/i
+handler.command = new RegExp
 
+
+handler.fail = null
+handler.exp = 100
 module.exports = handler
