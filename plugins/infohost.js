c7ce9b8e23ebdfafad23f9624bbab563d89fb23c
Adolfo Griego <104970935+AdolfoGriego@users.noreply.github.com>
2022-11-21 01:30:25 -0600

    infohost.js

--- plugins/infohost.js
+++ plugins/infohost.js
@@ -2,6 +2,9 @@ let handler = async (m, { conn }) => {
 m.reply(`
 *HOLA BUSCAS INSTALAR EL BOT 24/7 AQUI TE DEJO EL ENLACE PARA QUE ESTES AL CORRIENTE*
 https://chat.whatsapp.com/IE9bL1KB1laDzB7tZCB0WR
+
+*GRUPO DE AMISTAD*
+https://chat.whatsapp.com/IUNdYngu5AlCJFHuHHuen4
 `)
 }
 handler.command = /^(infohost)$/i
